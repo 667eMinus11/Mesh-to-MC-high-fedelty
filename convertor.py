5bde957cdd75df3b82f029d86516f3d8e4c3b4cf
Elsa002 <elsa002.linux@gmail.com>
2023-04-08 23:51:26 +0300

    Improve convertor API

--- convertor.py
+++ convertor.py
@@ -2,10 +2,6 @@ from abc import ABC, abstractmethod
 
 
 class MeshConvertor(ABC):
-    def __init__(self, stl_file: str, voxelize: float):
-        self.stl_file = stl_file
-        self.voxelize = voxelize
-
     @abstractmethod
-    def convert(self):
+    def convert(self, stl_file: str, voxelize: float):
         ...
--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
 import sys
-from typing import Type
 
 from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
 
@@ -13,12 +12,16 @@ from convertor import MeshConvertor
 
 
 class MainWindow(QWidget):
-    def __init__(self, convertor: Type[MeshConvertor], parent=None):
+    def __init__(self, convertor: MeshConvertor, parent=None):
+        self.convertor: MeshConvertor = convertor
+
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
+
         self.ui.browse_stl_button.clicked.connect(self.browse_stl)
         self.ui.browse_mesh_button.clicked.connect(self.browse_mesh)
+        self.ui.convert_button.clicked.connect(self.convert)
 
     def browse_stl(self):
         file_path = QFileDialog.getOpenFileName(self, "Open STL file", filter="STL (*.stl)")[0]
@@ -28,12 +31,16 @@ class MainWindow(QWidget):
         file_path = QFileDialog.getOpenFileName(self, "Open MESH file", filter="MESH (*.mesh)")[0]
         self.ui.mesh_line_edit.setText(file_path)
 
+    def convert(self):
+        self.convertor.convert(self.ui.stl_line_edit.text(), self.ui.voxelize_spin_box.value())
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     class fake_convertor(MeshConvertor):
-        def convert(self):
-            return super().convert()
-    main_window = MainWindow(fake_convertor)
+        def convert(self, stl_file: str, voxelize: float):
+            print(f"stl: {stl_file}")
+            print(f"voxelize: {voxelize}")
+    main_window = MainWindow(fake_convertor())
     main_window.show()
     sys.exit(app.exec())
